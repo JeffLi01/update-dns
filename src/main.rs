2ba4544493efc87337640ce970050f8b8015cee5
Jeff Li <lijinfeng01@ieisystem.com>
2025-03-27 06:58:48 +0800

    feat: get ip when not specified
    Signed-off-by: Jeff Li <lijinfeng01@ieisystem.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::io;
+use std::net::{IpAddr, SocketAddr, TcpStream};
+
 use base64::{Engine, prelude::BASE64_STANDARD};
 use clap::Parser;
 use dns_update::{DnsRecord, DnsUpdater, TsigAlgorithm};
+use log::debug;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -14,7 +18,14 @@ pub struct Args {
     #[arg(short, long)]
     pub name: Option<String>,
     #[arg(short, long)]
-    pub ip: String,
+    pub ip: Option<String>,
+}
+
+fn get_local_ip_for_remote_ip(remote_addr: &str) -> io::Result<IpAddr> {
+    let remote: SocketAddr = remote_addr.parse().unwrap();
+    let stream = TcpStream::connect(&remote)?;
+    let local_addr = stream.local_addr()?;
+    Ok(local_addr.ip())
 }
 
 #[tokio::main]
@@ -24,6 +35,19 @@ async fn main() {
         .name
         .unwrap_or_else(|| hostname::get().unwrap().to_string_lossy().to_string());
     debug!("hostname: {:?}", hostname);
+    let ip = match args.ip {
+        Some(ip) => ip.parse().expect("IP should be parsed succeffully"),
+        None => {
+            let remote = format!("{}:{}", args.server, 53);
+            match get_local_ip_for_remote_ip(&remote) {
+                Ok(ip) => ip,
+                Err(err) => {
+                    panic!("failed to get IP: {err}");
+                },
+            }
+        },
+    };
+    debug!("ip: {:?}", ip);
     // Create a new RFC2136 client
     let tsig = std::env::var("DNS_TSIG_KEY").expect("env variable `DNS_TSIG_KEY` should be set");
     let sig = BASE64_STANDARD.decode(tsig).unwrap();
@@ -35,12 +59,14 @@ async fn main() {
     client.delete(&hostname, &args.origin).await.unwrap();
 
     // Create a new A record
+    let record = match ip {
+        IpAddr::V4(ipv4_addr) => DnsRecord::A { content: ipv4_addr },
+        IpAddr::V6(ipv6_addr) => DnsRecord::AAAA { content: ipv6_addr },
+    };
     client
         .update(
             &hostname,
-            DnsRecord::A {
-                content: args.ip.parse().expect("IP should be parsed succeffully"),
-            },
+            record,
             300,
             args.origin,
         )
