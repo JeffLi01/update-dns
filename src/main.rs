4288c463eec355381ab33a36d9739b42852b506e
Jeff Li <lijinfeng01@ieisystem.com>
2025-03-28 17:31:47 +0800

    feat: update multiple name at once
    Signed-off-by: Jeff Li <lijinfeng01@ieisystem.com>

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::net::{IpAddr, SocketAddr, TcpStream};
 use base64::{Engine, prelude::BASE64_STANDARD};
 use clap::{ArgAction, Parser};
 use dns_update::{DnsRecord, DnsUpdater, TsigAlgorithm};
-use log::debug;
+use log::{error, info, debug};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -16,7 +16,7 @@ pub struct Args {
     #[arg(short, long)]
     pub key: String,
     #[arg(short, long)]
-    pub name: Option<String>,
+    pub names: Vec<String>,
     #[arg(short, long)]
     pub ip: Option<String>,
     #[arg(short, long, action = ArgAction::Count)]
@@ -35,15 +35,16 @@ async fn main() {
     let args = Args::parse();
     init_logger(args.verbose);
 
-    let hostname = args
-        .name
-        .unwrap_or_else(|| hostname::get().unwrap().to_string_lossy().to_string());
-    let fqdn = format!("{}.{}", hostname, args.origin);
-    debug!("fqdn: {:?}", fqdn);
-    let ip = match args.ip {
+    let names = if args.names.is_empty() {
+        let name = hostname::get().unwrap().to_string_lossy().to_string();
+        &vec![name]
+    } else {
+        &args.names
+    };
+    let ip = match &args.ip {
         Some(ip) => ip.parse().expect("IP should be parsed succeffully"),
         None => {
-            let remote = format!("{}:{}", args.server, 53);
+            let remote = format!("{}:{}", &args.server, 53);
             match get_local_ip_for_remote_ip(&remote) {
                 Ok(ip) => ip,
                 Err(err) => {
@@ -52,32 +53,38 @@ async fn main() {
             }
         }
     };
-    debug!("ip: {:?}", ip);
-    // Create a new RFC2136 client
-    let tsig = std::env::var("DNS_TSIG_KEY").expect("env variable `DNS_TSIG_KEY` should be set");
+    debug!("local ip: {:?}", ip);
+    let tsig =
+        std::env::var("DNS_TSIG_KEY").expect("env variable `DNS_TSIG_KEY` should be set");
     let sig = BASE64_STANDARD.decode(tsig).unwrap();
+    // Create a new RFC2136 client
     let client =
-        DnsUpdater::new_rfc2136_tsig(args.server, args.key, sig, TsigAlgorithm::HmacSha256)
+        DnsUpdater::new_rfc2136_tsig(&args.server, &args.key, sig, TsigAlgorithm::HmacSha256)
             .unwrap();
 
-    // Delete the record
-    client.delete(&fqdn, &args.origin).await.unwrap();
+    for name in names {
+        let fqdn = format!("{}.{}", name, &args.origin);
+        debug!("fqdn: {:?}", fqdn);
 
-    // Create a new A record
-    let record = match ip {
-        IpAddr::V4(ipv4_addr) => DnsRecord::A { content: ipv4_addr },
-        IpAddr::V6(ipv6_addr) => DnsRecord::AAAA { content: ipv6_addr },
-    };
-    client
-        .update(&fqdn, record, 300, args.origin)
-        .await
-        .unwrap();
+        // Create a new A record
+        let record = match ip {
+            IpAddr::V4(ipv4_addr) => DnsRecord::A { content: ipv4_addr },
+            IpAddr::V6(ipv6_addr) => DnsRecord::AAAA { content: ipv6_addr },
+        };
+        if let Err(err) = client.update(&fqdn, record, 300, &args.origin).await {
+            error!("failed to update record {fqdn}: {err}");
+            continue;
+        } else {
+            info!("succeed to update record {fqdn} -> {ip}");
+        }
+    }
 }
 
 fn init_logger(level: u8) {
     let filter_level = match level {
-        0 => log::LevelFilter::Info,
-        1 => log::LevelFilter::Debug,
+        0 => log::LevelFilter::Warn,
+        1 => log::LevelFilter::Info,
+        2 => log::LevelFilter::Debug,
         _ => log::LevelFilter::Trace,
     };
 
